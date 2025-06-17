057a77afeded56b0bd43b98806bf659ada461378
wols616 <wilber.rivas1@catolica.edu.sv>
2025-06-16 20:44:17 -0600

    cambios del build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
+  },
+  build: {
+    target: "esnext", // o puedes usar una versión específica: 'es2022'
   },
 });
